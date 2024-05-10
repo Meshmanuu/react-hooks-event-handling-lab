198dc3f8f37c7ebf5ad75027bf6d7eafc751954b
Meshmanuu <meshanyangu@gmail.com>
2024-05-10 19:53:58 +0300

    lab done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
-// Code Keypad Component Here
+// components/Keypad.js
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      {/* Add the input field with the type set to "password" and the event handler */}
+      <input type="password" onChange={() => console.log('Entering password...')} />
+    </div>
+  );
 }
 
 export default Keypad;
